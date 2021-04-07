f6c6c6cc48e3ec8d1f9c8e22b15a515d91979c2b
Thomas Chaplin <tomrchaplin@gmail.com>
2021-04-07 14:45:35 +0100

    Read graph with attributes

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ A collection of useful MATLAB functions and scripts that I like to have on my pa
 * `ifig` - a script for writing a figure to file (eps/png) in an interactive manner
 * `persForLoop` - a function for writing nested for loop which save state on each iterations of the loop
 * `persForLoopSplitSaves` - as above but output of each iteration is saved as a separate file and combined later
+* `read_graph_with_attrs` - read a graph exported in `edgelist` format via `igraph` alongside an attribute file with a similar name
---
+++ read_graph_with_attrs.m
@@ -0,0 +1,37 @@
+function G = read_graph_with_attrs(filename,varargin)
+	% Handle options
+	directed = ismember('directed', varargin);
+	selfloops = ismember('selfloops', varargin);
+
+	% Read atribute file
+	attr_filename = sprintf('%s.attrs.csv', filename);
+	disp(attr_filename)
+	attrT = readtable(attr_filename);
+
+	% Init graph
+	N = height(attrT);
+	if directed
+		G = digraph();
+	else
+		G = graph();
+	end
+	G = addnode(G, N);
+
+	% Add atributes
+	num_attrs = length(attrT.Properties.VariableNames)
+	for i=2:num_attrs
+		attr_name = attrT.Properties.VariableNames{i}
+		G.Nodes.(attr_name) = attrT.(attr_name)
+	end
+
+	% Add edges
+	edgeT = readtable(filename);
+	s = edgeT.Var1 + 1;
+	t = edgeT.Var2 + 2;
+	G = addedge(G, s, t);
+	
+	% Remove any self loops
+	if ~selfloops
+		G = rmedge(G, 1:N, 1:N);
+	end
+end
